c2dcee91a612085cceea4a467016734daa8c4d42
Khushal Malik <khshlmlk@gmail.com>
2025-11-17 11:27:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 const app = require("./app");
 
-app.get("/", (req, res) => {
-  res.send("Hello from CI/CD Pipeline!");
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App running on port ${port}`));
-
-module.exports = app;
