f314918cf14c19f799c2670b5046b7d6da0ff92d
Khushal Malik <khshlmlk@gmail.com>
2025-11-17 11:02:19 +0530

    Create app.test.js

---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const app = require("../src/index");
+
+describe("GET /", () => {
+  it("should return hello message", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+  });
+});
